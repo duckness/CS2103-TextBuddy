1bf7b11a8768c60dac3a14c4aa48c8971d150988
Bjorn Lim <mail@bjornl.im>
2014-08-26 01:26:33 +0800

    Discovered enums, (hopefully) final refactor of TextBuddy.java?

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -1,11 +1,13 @@
 /**
+ * =====NOTES==============
  * CS2103T (AY2014/15 Sem1) 
  * CE2 - TextBuddy++ 
- * Author: Bjorn Lim 
- * Matric: A0116538A 
+ * A0116538A
  * Tutorial ID: T09 
- * Description: This program performs operations on text in a file
+ * ====/NOTES==============
  */
+
+import static org.fusesource.jansi.Ansi.ansi;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,17 +15,80 @@ import java.nio.file.Files;
 import java.util.Scanner;
 
 import org.fusesource.jansi.AnsiConsole;
-import static org.fusesource.jansi.Ansi.ansi;
 
+/**
+ * TextBuddy is a program which manipulates text in a file. The file to be edited
+ * is specified in the command line parameters, and will be created if asked for.
+ * Adding, deleting, clearing, displaying, sorting, and searching is possible. 
+ * New entries will be added to the end of the file, which will be saved whenever
+ * an input changes the contents of the file. 
+ * 
+ * Example command format:
+ *     c:> TextBuddy mytextfile.txt  (OR c:>java  TextBuddy mytextfile.txt)
+ *     Welcome to TextBuddy. mytextfile.txt is ready for use
+ *     command: add little brown fox
+ *     added to mytextfile.txt: “little brown fox”
+ *     command: display
+ *     1. little brown fox
+ *     command: add jumped over the moon
+ *     added to mytextfile.txt: “jumped over the moon”
+ *     command: display
+ *     1. little brown fox
+ *     2. jumped over the moon
+ *     command: delete 2
+ *     deleted from mytextfile.txt: “jumped over the moon”
+ *     command: display
+ *     1. little brown fox
+ *     command: clear
+ *     all content deleted from mytextfile.txt
+ *     command: display
+ *     mytextfile.txt is empty
+ *     command: exit
+ *     c:> 
+ * 
+ * @author Bjorn Lim
+ */
 public class TextBuddy {
+    
+    private static final String ADD_ = "Added to \"%1$s\": \"%2$s\".\n";
+    private static final String BAD_ACCESS_ = "Unable to access file.\n";
+    private static final String BAD_COMMAND_ = "Please input a valid command.\n";
+    private static final String BAD_DELETE_ = "Please specify a line number that exists.\n";
+    private static final String BAD_FILE_ =  "File does not exist.\n";
+    private static final String CLEAR_ = "All content deleted from \"%1$s\".\n";
+    private static final String COMMAND_ = "Command: ";
+    private static final String DELETE_ = "Deleted from \"%1$s\": \"%2$s\".\n";
+    private static final String NEW_FILE_ = "Would you like to create a new file? [Y/N]: ";
+    private static final String SORT_ = "All lines sorted alphabetically in \"%1$s\".\n";
+    private static final String WELCOME_ = "Welcome to TextBuddy. \"%1$s\" is ready for use.\n";
 
+    // Possible user command types for the main program
+    enum USER_COMMAND {
+        DISPLAY, ADD, DELETE, CLEAR, SORT, SEARCH, EXIT
+    }
+    
+    // Possible user command types for Y/N questions
+    enum YES_NO {
+        Y, N
+    }
+    // The object that will be holding all of our information
     private static txtFile inputFile_;
+    
+    // Declared outside for DRYness
+    private static Scanner sc_ = new Scanner(System.in);
 
     public static void main(String[] args) {
         inputFile_ = processInput(args);
-        printMessage("welcome");
+        printMessage(WELCOME_, inputFile_.getName());
         processCommands();
     }
+    
+    /**
+     * This method ensures that the user enters a valid input before making a txtFile
+     * 
+     * @param args Is the user's input
+     * @return Returns a txtFile if the user input is valid
+     */
 
     // check for IO errors before creating a txtFile
     public static txtFile processInput(String[] args) {
@@ -31,193 +96,226 @@ public class TextBuddy {
         checkForMissingFile(args);
         return new txtFile(args[0]);
     }
+    
+    /**
+     * This method prints strings plus some values to be inserted if needed
+     * 
+     * @param str Is the string to be printed
+     * @param args Are the additional values to be added if needed
+     */
+    private static void printMessage(String str, Object... args) {
+        System.out.print(String.format(str, args));
+    }
+    
+    /**
+     * This method determines what command the user is performing and executes it 
+     */
+    public static void processCommands() {  
+        while (true) {
+            printMessage(COMMAND_);
+            switch (USER_COMMAND.valueOf(readWord())) {
 
-    // ensure that the user provides arguments
-    public static void checkForMissingArg(String[] args) {
-        if (args.length == 0) {
-            printMessage("noFile");
-            System.exit(1);
+                case DISPLAY :
+                    commandDisplay();
+                    break;
+
+                case ADD :
+                    commandAdd();
+                    break;
+
+                case DELETE :
+                    commandDelete();
+                    break;
+
+                case CLEAR :
+                    commandClear();
+                    break;
+
+                case SORT :
+                    commandSort();
+                    break;
+
+                case SEARCH :
+                    commandSearch();
+                    break;
+
+                case EXIT :
+                    commandExit();
+                    break;
+
+                default :
+                    commandIsBad();
+                    break;
+            }
         }
     }
+    
+    /**
+     * This method exits the program, throwing an error, if no file is found
+     * 
+     * @param args Is the user's input
+     */
+
+    // ensure that the user provides arguments
+    private static void checkForMissingArg(String[] args) {
+        if (args.length == 0) {
+            throw new Error(BAD_FILE_);
+        }
+    }
+    
+    /**
+     * This method checks if the user supplied filename matches a file in the
+     * directory. If not, ask the user if he wants to make a new file with that name.
+     * 
+     * @param args Is the user's input
+     */
 
     // ensure that the file exists, otherwise ask if a new file is wanted
-    public static void checkForMissingFile(String[] args) {
+    private static void checkForMissingFile(String[] args) {
         File checkFile = new File(args[0]);
         if (!checkFile.exists()) {
-            printMessage("noFile");
-            printMessage("newFile");
+            printMessage(BAD_FILE_);
+            printMessage(NEW_FILE_);
             doesUserWantNewFile(checkFile);
         }
     }
 
-    // ask if the user wants to make a new file as it does not exist
-    public static void doesUserWantNewFile(File newFile) {
-        Scanner sc = new Scanner(System.in);
-
+    
+    /**
+     * This method asks if the user wants to make a new file
+     * 
+     * @param newFile Is the file to be created
+     */
+    private static void doesUserWantNewFile(File newFile) {
         outerLoop: while (true) {
-            switch (sc.next().toUpperCase()) {
+            switch (YES_NO.valueOf(readWord())) {
 
-                case "Y" :
-                    sc.close();
+                case Y :
                     createFile(newFile);
                     break outerLoop;
 
-                case "N" :
-                    sc.close();
-                    System.exit(2);
+                case N :
+                    System.exit(1);
                     break;
 
                 default :
-                    printMessage("invalidCommand");
+                    commandIsBad();
                     break;
             }
         }
     }
 
+    /**
+     * This method creates a new file, throwing an error if the program 
+     * is unable to do so.
+     * 
+     * @param newFile Is the file to be created
+     */
     // try to create a new file if possible
-    public static void createFile(File newFile) {
+    private static void createFile(File newFile) {
         try {
             Files.createFile(newFile.toPath());
         } catch (IOException e) {
-            printMessage("noAccess");
-            System.exit(3);
+            throw new Error(BAD_ACCESS_);
         }
     }
 
-    // print the messages in this class
-    public static void printMessage(String str) {
-        switch (str) {
-            case "noFile" :
-                System.out.println("File does not exist.");
-                break;
 
-            case "newFile" :
-                System.out.print("Would you like to create a new file? [Y/N]");
-                break;
-
-            case "invalidCommand" :
-                System.out.println("Please input a valid command.");
-                break;
-
-            case "noAccess" :
-                System.out.println("Unable to access file.");
-                break;
-
-            case "welcome" :
-                System.out.println("Welcome to TextBuddy. \""
-                        + inputFile_.getName() + "\" is ready for use.");
-                break;
-
-            case "command" :
-                System.out.print("Command: ");
-                break;
-
-            case "display" :
-                System.out.print(inputFile_.toString());
-                break;
-
-            case "add" :
-                System.out.println("Added to \"" + inputFile_.getName()
-                        + "\": \"" + inputFile_.getLast() + "\".");
-                break;
-
-            case "delete" :
-                System.out.println("Deleted from \"" + inputFile_.getName()
-                        + "\": \"" + inputFile_.getLastDeleted() + "\".");
-                break;
-
-            case "noDelete" :
-                System.out.println("Please specify a line number that exists.");
-                break;
-                
-            case "clear" :
-                System.out.println("All content deleted from \""
-                        + inputFile_.getName() + "\".");
-                break;
-
-            case "sort" :
-                System.out.println("All lines sorted alphabetically in \""
-                        + inputFile_.getName() + "\".");
-                break;
-
-            case "search" :
-                AnsiConsole.systemInstall();
-                System.out.print(ansi().render(inputFile_.getSearchResults()));
-                AnsiConsole.systemUninstall();
-                break;
-
-            default :
-                break;
-
-        }
+    /**
+     * This method deals with what to do when a user inputs an invalid command
+     */
+    private static void commandIsBad() {
+        printMessage(BAD_COMMAND_);
     }
 
-    // process the user's inputs
-    public static void processCommands() {
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            printMessage("command");
-            String cmd = sc.next().toLowerCase();
-
-            switch (cmd) {
-
-                case "display" :
-                    printMessage("display");
-                    break;
-
-                case "add" :
-                    inputFile_.add(readPhrase(sc));
-                    printMessage("add");
-                    inputFile_.save();
-                    break;
-
-                case "delete" :
-                    deleteLine(sc.nextInt());
-                    inputFile_.save();
-                    break;
-
-                case "clear" :
-                    inputFile_.clear();
-                    printMessage("clear");
-                    inputFile_.save();
-                    break;
-
-                case "sort" :
-                    inputFile_.sort();
-                    printMessage("sort");
-                    inputFile_.save();
-                    break;
-
-                case "search" :
-                    inputFile_.search(readPhrase(sc));
-                    printMessage("search");
-                    break;
-
-                case "exit" :
-                    sc.close();
-                    System.exit(0);
-                    break;
-
-                default :
-                    printMessage("invalidCommand");
-                    break;
-            }
-        }
+    /**
+     * This method exits the program
+     */
+    private static void commandExit() {
+        AnsiConsole.systemUninstall();
+        System.exit(0);
     }
 
-    // removes leading character, in our case, the leading whitespace
-    public static String readPhrase(Scanner sc) {
-        return sc.nextLine().substring(1);
+    /**
+     * This method looks for a phrase in the txtFile
+     */
+    private static void commandSearch() {
+        AnsiConsole.systemInstall(); // needed to render text in colors
+        inputFile_.search(readPhrase());
+        printMessage(ansi().render(inputFile_.getSearchResults()).toString());
+    }
+
+    /**
+     * This method sorts the lines in the txtFile
+     */
+    private static void commandSort() {
+        inputFile_.sort();
+        printMessage(SORT_, inputFile_.getName());
+        inputFile_.save();
+    }
+
+    /**
+     * This method clears all the lines in the txtFile
+     */
+    private static void commandClear() {
+        inputFile_.clear();
+        printMessage(CLEAR_, inputFile_.getName());
+        inputFile_.save();
+    }
+
+    /**
+     * This method deletes a line in the txtFile
+     */
+    private static void commandDelete() {
+        deleteLine(sc_.nextInt());
+        inputFile_.save();
+    }
+
+    /**
+     * This method adds a line in the txtFile
+     */
+    private static void commandAdd() {
+        inputFile_.add(readPhrase());
+        printMessage(ADD_, inputFile_.getName(), inputFile_.getLast());
+        inputFile_.save();
+    }
+
+    /**
+     * This method prints all the lines in the txtFile
+     */
+    private static void commandDisplay() {
+        printMessage(inputFile_.toString());
+    }
+
+    /**
+     * This method reads a word and changes it to uppercase
+     * 
+     * @return Returns the input word in uppercase
+     */
+    private static String readWord() {
+        return sc_.next().toUpperCase();
     }
     
-    // ensure that the line to be deleted exists
-    public static void deleteLine(int lineNum) {
+    /**
+     * This method removes the leading character, in our case, the leading whitespace
+     *  
+     * @return Returns the input line without leading whitespace
+     */
+    private static String readPhrase() {
+        return sc_.nextLine().substring(1);
+    }
+    
+    /**
+     * This method ensures that the line to be deleted exists
+     * 
+     * @param lineNum Is the line number to be deleted
+     */
+    private static void deleteLine(int lineNum) {
         if (inputFile_.size() <= lineNum) {
-            printMessage("noDelete");
+            printMessage(BAD_DELETE_);
         } else {
             inputFile_.delete(lineNum);
-            printMessage("delete");
+            printMessage(DELETE_, inputFile_.getName(), inputFile_.getLastDeleted());
         }
     }
 }
