7617d996191c32f411904fa7085d854dc4af6163
Bjorn Lim <mail@bjornl.im>
2014-09-16 19:29:03 +0800

    add JUnit test for toString method in TextFile.java

--- test/textbuddy/TextFileTest.java
+++ test/textbuddy/TextFileTest.java
@@ -1,14 +1,18 @@
 package textbuddy;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class TextFileTest {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
-    }
+    private static final String TEST_FILE = "mytestfile.txt";
+    private static final String TEST_PATH = "src/" + TEST_FILE;
 
+    @Test
+    public void testToString() {
+        TextFile tester = new TextFile(TEST_PATH);
+        assertEquals(TEST_FILE + "is & must be empty before testing can start",
+                      "\"" + TEST_FILE + "\" is empty.%n",tester.toString());
+    }
 }
