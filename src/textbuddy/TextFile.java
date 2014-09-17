99022a186168acf1246f74489364ce40560a714f
Bjorn Lim <mail@bjornl.im>
2014-09-17 13:41:16 +0800

    pass the modified test cases for search()

--- src/textbuddy/TextFile.java
+++ src/textbuddy/TextFile.java
@@ -103,7 +103,7 @@ class TextFile {
     public String search(String searchString) {
         for (int i = 0; i < lines_.size(); i++) {
             if (lines_.get(i).contains(searchString)){
-                return (i + 1) + ". " + lines_.get(i);
+                return (i + 1) + ". " + lines_.get(i) + "%n";
             }
         }
         return null;
